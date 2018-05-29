ffa8e3f6761c1145bf79f675eeb954a99a2ce146
Ruslan Sukharev <notebook1v@gmail.com>
2018-05-29 10:54:41 +0500

    refactor askIsEven.js

--- src/askIsEven.js
+++ src/askIsEven.js
@@ -5,15 +5,18 @@ const askIsEven = (username, count = 0, min = 1, max = 100) => {
     console.log(`Congratulations, ${username}!`);
     return `Congratulations, ${username}!`;
   }
+
   const randomNum = Math.floor(Math.random() * ((max - min) + min));
   console.log(`Question: ${randomNum}`);
-  const userAnswer = readlineSync.question('Your answer: ');
   const isEven = randomNum % 2 === 0;
-  if ((userAnswer === 'yes' && isEven) || (userAnswer === 'no' && !isEven)) {
+  const correctAnswer = isEven ? 'yes' : 'no';
+  const userAnswer = readlineSync.question('Your answer: ');
+
+  if (userAnswer === correctAnswer) {
     console.log('Correct!');
     return askIsEven(username, count + 1);
   }
-  const correctAnswer = isEven ? 'yes' : 'no';
+
   console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
   console.log(`Let's try again, ${username}!`);
   return `Let's try again, ${username}!`;
