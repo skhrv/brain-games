a80eca6c84e3b6c651a1885a73dd16104ec5ec87
Ruslan Sukharev <notebook1v@gmail.com>
2018-05-30 17:54:44 +0500

    fix

--- src/askIsEven.js
+++ src/askIsEven.js
@@ -1,27 +1,24 @@
 import readlineSync from 'readline-sync';
 import { greetings, getRandomNum, checkAnswer } from './utils';
 
+const rule = 'Answer "yes" if number even otherwise answer "no".';
+const numberOfCorrectAnswer = 3;
+const isEven = question => (question % 2 === 0 ? 'yes' : 'no');
 
 const askIsEven = () => {
-  const rules = 'Answer "yes" if number even otherwise answer "no".';
-  const username = greetings(rules);
+  const username = greetings(rule);
 
   const iter = (count = 0) => {
-    if (count === 3) {
+    if (count === numberOfCorrectAnswer) {
       console.log(`Congratulations, ${username}!`);
-      return true;
+      return undefined;
     }
-    const randomNum = getRandomNum();
-    console.log(`Question: ${randomNum}`);
-    const isEven = randomNum % 2 === 0;
-    const correctAnswer = isEven ? 'yes' : 'no';
+    const question = getRandomNum();
+    console.log(`Question: ${question}`);
+    const correctAnswer = isEven(question);
     const userAnswer = readlineSync.question('Your answer: ');
-
-    const result = checkAnswer(username, userAnswer, correctAnswer);
-    if (result) {
-      return iter(count + 1);
-    }
-    return false;
+    const result = checkAnswer(username, userAnswer, correctAnswer) ? iter(count + 1) : undefined;
+    return result;
   };
   return iter();
 };
