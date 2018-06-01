5a64dcb512bd417d5bc7d71be0309c3b12387734
Ruslan Sukharev <39522630+skhrv@users.noreply.github.com>
2018-06-01 08:46:20 +0500

    refactor brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -5,13 +5,13 @@ import startGame from '..';
 const rule = 'Find the greatest common divisor of given numbers.';
 
 const gcd = (num1, num2) => {
-  if (num1 > num2) {
-    if (num1 === 0 || num2 === 0) {
-      return String(num1 + num2);
-    }
-    return gcd(num2, num1 % num2);
+  if (num1 < num2) {
+    return gcd(num1, num2 % num1);
   }
-  return gcd(num1, num2 % num1);
+  if (num1 === 0 || num2 === 0) {
+    return String(num1 + num2);
+  }
+  return gcd(num2, num1 % num2);
 };
 
 const getQandA = () => {
