54ff44e69eaa9db03a27f3a093275c43b3c1b766
Ruslan Sukharev <notebook1v@gmail.com>
2018-06-01 21:07:28 +0500

    add new game brain-progression

---
+++ src/games/brain-progression.js
@@ -0,0 +1,32 @@
+import { cons } from 'hexlet-pairs';
+import getRandomNum from '../utils';
+import startGame from '..';
+
+const rule = 'What number is missing in this progression?';
+const lengthProgression = 10;
+
+const getProgression = () => {
+  const startProgression = getRandomNum();
+  const stepInProgression = getRandomNum(1, 10);
+  const progression = [startProgression];
+  let currentNumInProgression = startProgression;
+  for (let i = 1; i <= lengthProgression; i += 1) {
+    currentNumInProgression += stepInProgression;
+    progression.push(currentNumInProgression);
+  }
+  return progression;
+};
+const getQandA = () => {
+  const progression = getProgression();
+  const indexOfHiddenNum = getRandomNum(0, 9);
+  const answer = String(progression[indexOfHiddenNum]);
+  progression[indexOfHiddenNum] = '..';
+  let question = '';
+  for (let i = 0; i < progression.length; i += 1) {
+    question += `${progression[i]} `;
+  }
+  return cons(question, answer);
+};
+
+startGame(rule, getQandA);
+
